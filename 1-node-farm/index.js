52bc4374bd1fd067e22883e616ce7a38847ae451
027 <anbschool0027@027s-Mac-mini.local>
2023-09-29 19:58:52 +0700

    Routing

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,8 +1,53 @@
 const fs = require("fs");
+const http = require("http");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+///File
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File Written");
+//blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File Written");
+
+//Non-blocking, asynchronous way
+// fs.readFile(`./txt/start.txt`, "utf-8", (err, data1) => {
+//   if (err) return console.log("ERROR!");
+
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile(
+//         "./txt/final.txt",
+//         `${data2} \n ${data3}`,
+//         "utf-8",
+//         (err) => {
+//           console.log("Your file has been written");
+//         }
+//       );
+//     });
+//   });
+// });
+// console.log("Will read file!");
+
+///END////
+
+///SERVER///
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "./product") {
+    res.end("Hello from the server!");
+  } else {
+    res.writeHead(404);
+    res.end("Page not found!");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});

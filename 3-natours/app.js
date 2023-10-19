7455442212ce10a6fae3d072a692fea871059fc1
027 <anbschool0027@027s-Mac-mini.local>
2023-10-19 16:38:43 +0700

    adding json data to project

--- 3-natours/app.js
+++ 3-natours/app.js
@@ -2,15 +2,17 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Hello from the server side!", app: "Natours" });
-});
+// app.get("/", (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: "Hello from the server side!", app: "Natours" });
+// });
 
-app.post("/", (req, res) => {
-  res.send("You can post to this endpoint...");
-});
+// app.post("/", (req, res) => {
+//   res.send("You can post to this endpoint...");
+// });
+
+app.get("/api/v1/tours", (req, res) => {});
 
 const port = 3000;
 app.listen(port, () => {

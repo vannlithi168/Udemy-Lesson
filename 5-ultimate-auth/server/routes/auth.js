dd235e01bb8df23ee6c746d4f6e4cf886d62b0a3
027 <anbschool0027@027s-Mac-mini.local>
2023-11-14 09:44:37 +0700

    create controller

--- 5-ultimate-auth/server/routes/auth.js
+++ 5-ultimate-auth/server/routes/auth.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-  res.json({
-    data: "You hit sign up end point",
-  });
-});
+// import controller
+const { signup } = require("../controllers/auth");
+
+router.get("/signup", signup);
 
 module.exports = router;

0229cf1e4eb780dfbbb17f4239b8af9e098f3340
027 <anbschool0027@027s-Mac-mini.local>
2023-11-14 18:10:19 +0700

    finish signing user simple flow

--- 5-ultimate-auth/server/controllers/auth.js
+++ 5-ultimate-auth/server/controllers/auth.js
@@ -1,6 +1,33 @@
-exports.signup = (req, res) => {
-  console.log("REQ BODY ON SIGNUP", req.body);
-  res.json({
-    data: "You hit sign up end point",
-  });
-};
+const User = require("../models/user");
+
+// exports.signup = async (req, res) => {
+//   const { name, email, password } = req.body;
+
+//   try {
+//     // Check if the user already exists
+//     const existingUser = await User.findOne({ email });
+
+//     if (existingUser) {
+//       return res.status(400).json({
+//         error: "Email is taken",
+//       });
+//     }
+
+//     // Create a new user
+//     const newUser = new User({ name, email, password });
+
+//     // Save the new user
+//     await newUser.save();
+
+//     res.json({
+//       message: "Signup success! Please signin!",
+//     });
+//   } catch (error) {
+//     console.log("SIGNUP ERROR", error);
+//     return res.status(400).json({
+//       error: error.message,
+//     });
+//   }
+// };
+
+exports.signup = async (req, res) => {};

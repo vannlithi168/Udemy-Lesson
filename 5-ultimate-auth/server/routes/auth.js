dcc0d45c8e5f705c2f6a1920e8840f37c5efe980
vannlithi168 <vannlithi168@gmail.com>
2023-11-22 22:26:43 +0700

    testing

--- 5-ultimate-auth/server/routes/auth.js
+++ 5-ultimate-auth/server/routes/auth.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-// import controller
 const { signup } = require("../controllers/auth");
 
-//import validators
 const { userSignupValidator } = require("../validators/auth");
 const { runValidation } = require("../validators");
 
